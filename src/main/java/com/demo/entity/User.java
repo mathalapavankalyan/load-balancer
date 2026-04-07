5771d7223b4f7bd09aa344797f25365c9cb6150a
pavankalyan <mathalapavankalyan@gmail.com>
2026-04-07 08:06:14 +0530

    Initial commit - Spring Boot load balancer project

---
+++ src/main/java/com/demo/entity/User.java
@@ -0,0 +1,36 @@
+package com.demo.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
